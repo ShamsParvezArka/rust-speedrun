46965b40440d32a1c9f24ef86d3ba99b4b7439c0
Arka <38325426+ShamsParvezArka@users.noreply.github.com>
2024-05-12 22:02:15 +0600

    Add files via upload

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    loop {
+        println!("Please input your guess");
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(x) => x,
+            Err(_) => {
+                println!("Please enter a number bitch");
+                continue;
+            },
+        };
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("TOO SMALL"),
+            Ordering::Greater => println!("TOO BIG"),
+            Ordering::Equal => {
+                println!("YOU WIN");
+                break;
+            },
+        }
+    }
+    
+}
